bb7f4dd6f308d7b444a56e0e105d0869594acde2
Filip Zlopaša <fico5zlopasa@gmail.com>
2025-12-26 10:45:46 +0100

    config: update neovim config

---
+++ nvim/.config/nvim/lua/set.lua
@@ -0,0 +1,24 @@
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+vim.opt.wrap = true
+
+vim.opt.scrolloff = 30
+
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+vim.opt.autoindent = true
+vim.opt.wildignore="*.o,*.obj,~*"
+--opcionalno
+vim.opt.clipboard=unnamedplus
+
+vim.g.mapleader = " "
+vim.g.netrw_liststyle = 3
+
+vim.g.blamer_delay = 250
+vim.g.netrw_winsize = 30
+
